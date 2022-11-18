125dc3e6f00fb337a645104ac61b0820d97c3674
Luka Vidakovic <luka.vidakovic@labsoft.dev>
2022-11-18 19:54:20 +0100

    Reorganized test for automation

--- TddBddDesignTroughTesting/test_str_calc.py
+++ TddBddDesignTroughTesting/test_str_calc.py
@@ -9,23 +9,24 @@ class TestStrCalc(unittest.TestCase):
 
     def test_should_return_zero_for_empty_string(self):
         """Testing scenario 1:"""
-        self.assertEqual(0, Calculator().add(""))
+        assert Calculator().add("") == 0
 
     def test_should_return_value_for_single_number(self):
         """Testing scenario 2:"""
-        self.assertEqual(5, Calculator().add("5"))
+        assert 5 == Calculator().add("5")
 
     def test_should_return_sum_of_multiple_numbers(self):
         """Testing Scenario 3:"""
-        self.assertEqual(3, Calculator().add("1, 2"))
+        assert 3 == Calculator().add("1, 2")
 
     def test_should_return_sum_of_multiple_numbers_with_newline_delimiter(self):
         """Testing Scenario 4:"""
-        self.assertEqual(6, Calculator().add("1\n2, 3"))
+        assert 6 == Calculator().add("1\n2, 3")
 
     def test_should_return_sum_of_multiple_numbers_with_user_defined_delimiter(self):
         """Testing Scenario 5:"""
-        self.assertEqual(3, Calculator().add("//;\n1;2", "//|;|\n"))
+        assert 3 == Calculator().add("//;\n1;2", "//|;|\n")
 
 
-unittest.main()
+if __name__ == "__main__":
+    unittest.main()  # pragma: no cover
