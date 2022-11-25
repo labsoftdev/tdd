81d75fee6888f009e51913e0e9aacb223dede494
Luka Vidakovic <luka.vidakovic@labsoft.dev>
2022-11-25 20:21:29 +0100

    EX5 added addition for fractions with different denominators

--- TddBddDesignTroughTesting/Exercise-5/fraction.py
+++ TddBddDesignTroughTesting/Exercise-5/fraction.py
@@ -5,17 +5,30 @@ class Fraction:
     """Fraction class"""
 
     def __init__(self, numerator, denominator=1):
-        self.numerator = numerator
-        self.denominator = denominator
+        gdc = self._gcd(numerator, denominator)
+        self.numerator = int(numerator / gdc)
+        self.denominator = int(denominator / gdc)
 
     def add(self, other):
         """Performing addition of two fractional numbers"""
-        num = self.numerator + other.numerator
-        den = self.denominator
-        return Fraction(num, den)
+        return Fraction(self._get_numerator(other), self._get_denominator(other))
 
     def __str__(self) -> str:
         ret_val = str(self.numerator)
         if self.denominator != 1:
             ret_val += "/" + str(self.denominator)
         return ret_val
+
+    def _get_numerator(self, other):
+        return self.numerator * other.denominator + other.numerator * self.denominator
+
+    def _get_denominator(self, other):
+        return self.denominator * other.denominator
+
+    def _gcd(self, num, den):
+        while num != den:
+            if num > den:
+                num -= den
+            else:
+                den -= num
+        return num
--- TddBddDesignTroughTesting/Exercise-5/test_fraction.py
+++ TddBddDesignTroughTesting/Exercise-5/test_fraction.py
@@ -9,9 +9,13 @@ class FractionTest(unittest.TestCase):
         """Scenario 1: Add two fraction with just numerator"""
         self.assertEqual("3", str(Fraction(1).add(Fraction(2))))
 
-    def test_should_add_two_fractional_numbers_with_the_different_denominators(self):
+    def test_should_add_two_fractional_numbers_with_the_same_denominators(self):
         """Scenario 2: Add two fractional numbers with the same denominators"""
         self.assertEqual("3/5", str(Fraction(1, 5).add(Fraction(2, 5))))
+
+    def test_should_add_two_fractional_numbers_with_the_different_denominators(self):
+        """Scenario 3: Add two fractional numbers with the different denominators"""
+        self.assertEqual("5/6", str(Fraction(1, 2).add(Fraction(1, 3))))
 
 
 if __name__ == "__main__":
